65b53b6fe9a3b8a112e77bdf37634ae50a16a4c2
frckbrice <maebrie2017@gmail.com>
2023-10-02 09:23:59 +0100

    building impelementation interface

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-import '../src/CSS/style.css'
+import '../src/CSS/style.css';
+import FullList from './model/FullListItem';
+import ListItem from './model/listItems';
+import ListTemplate from './template/listTemplate';
+
+
+i
---
+++ src/model/FullListItem.ts
@@ -0,0 +1,94 @@
+import ListItem from "./listItems";
+
+interface List {
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clearList(): void;
+  addItem(itemObj: ListItem): void;
+  removeItem(id: string): void;
+}
+
+// export default class FullList implements List {
+//   list: ListItem[];
+//   constructor() {
+//     this.list = [];
+//   }
+
+
+//   load(): void {
+//     //load from database
+//   }
+
+//   save(): void {
+//     localStorage.setItem('list', JSON.stringify(this.list));
+//   }
+
+//   clearList(): void {
+//     this.list = [];
+//   }
+
+//   addItem(itemObj: ListItem): void {
+//     this.list.push(itemObj);
+//   }
+
+//   removeItem(id: string): void {
+//     const index = this.list.findIndex((obj: ListItem) => obj.id === id);
+
+//     if(index > -1) {
+//       this.list.splice(index, 1);
+//     }
+
+//   }
+
+// }
+
+export default class FullList implements List {
+  
+  static instance: FullList = new FullList();
+
+  private constructor(private _list: ListItem[] = []) {}
+   //! This makes this fullList a singleton. so there will be only one instance.
+
+   get list(): ListItem [] {
+    return this._list;
+   }
+
+  load(): void {
+    const storedList: string | null = localStorage.getItem('list');
+    if(typeof storedList !== 'string')
+      return;
+
+    const parsedList: {_id: string, _item: string, _checked: boolean}[] = JSON.parse(storedList);
+
+    parsedList.forEach(itemObj => {
+      const newlistItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked);
+
+      FullList.instance.addItem(newlistItem);
+    });
+    
+  }
+
+  save(): void {
+    localStorage.setItem("list", JSON.stringify(this._list));
+  }
+
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+
+  removeItem(id: string): void {
+    const index = this._list.findIndex((obj: ListItem) => obj.id === id);
+
+    if (index > -1) {
+      this._list.splice(index, 1);
+      this.save();
+    }
+  }
+}
