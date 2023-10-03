f4aef642af410c8ca870b82d801957ed4611196d
frckbrice <maebrie2017@gmail.com>
2023-10-03 08:42:48 +0100

    adding, deleting items

--- src/model/listItems.ts
+++ src/model/listItems.ts
@@ -1,7 +1,45 @@
+// export interface Items {
+//   id: string;
+//   item: string;
+//   checked?: boolean
+// }
+
+// export default class ListItem implements Items {
+//   constructor(
+//     private _id: string = "",
+//     private _item: string = "",
+//     private _checked: boolean = false
+//   ) {}
+
+//   get id(): string {
+//     return this._id;
+//   }
+
+//   set id(value: string) {
+//     this._id = value;
+//   }
+
+//   get item(): string {
+//     return this._item;
+//   }
+
+//   set item(value: string) {
+//     this._item = value;
+//   }
+
+//   get checked(): boolean {
+//     return this._checked;
+//   }
+
+//   set checked(value: boolean) {
+//     this._checked = value;
+//   }
+// }
+
 export interface Items {
-  id: string;
-  item: string;
-  checked: boolean
+  id: string,
+  item: string,
+  checked?: boolean
 }
 
 export default class ListItem implements Items {
@@ -11,7 +49,7 @@ export default class ListItem implements Items {
     private _checked: boolean = false
   ) {}
 
-  get id(): string {
+  get id() {
     return this._id;
   }
 
@@ -19,15 +57,14 @@ export default class ListItem implements Items {
     this._id = value;
   }
 
-  get item(): string {
+  get item() {
     return this._item;
   }
 
   set item(value: string) {
     this._item = value;
   }
-
-  get checked(): boolean {
+  get checked() {
     return this._checked;
   }
 
